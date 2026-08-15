931c1abc5d0dc16001974a72055da5174fb91213
IsaiasDuron <isaiasduron17@unitec.edu>
2026-08-14 22:52:56 -0600

    mensaje

---
+++ ProyectoQ3Backend1/Services/FirebaseServices.cs
@@ -0,0 +1,21 @@
+﻿using Google.Cloud.Firestore;
+
+namespace ProyectoQ3Backend.Services;
+
+public class FirebaseService
+{
+    private readonly FirestoreDb _firestoreDb;
+
+    public FirebaseService()
+    {
+        var credentialPath = Path.Combine(
+            AppContext.BaseDirectory, "Config", "firebase-crendentials.json");
+
+        _firestoreDb = FirestoreDb.Create("proyecto-clase-2026");
+    }
+
+    public CollectionReference GetCollection(string collectionName)
+    {
+        return _firestoreDb.Collection(collectionName);
+    }
+}

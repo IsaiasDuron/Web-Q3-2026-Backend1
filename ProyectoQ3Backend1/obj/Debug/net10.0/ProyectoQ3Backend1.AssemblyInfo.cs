3ae8e6530693fc1bbba7c3764cf3f0e8dc3c6052
IsaiasDuron <isaiasduron17@unitec.edu>
2026-08-25 21:08:36 -0600

    agregando lo faltante

--- ProyectoQ3Backend1/obj/Debug/net10.0/ProyectoQ3Backend1.AssemblyInfo.cs
+++ ProyectoQ3Backend1/obj/Debug/net10.0/ProyectoQ3Backend1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoQ3Backend1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931c1abc5d0dc16001974a72055da5174fb91213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20cc554e1900f8c7407efce062a96b86116da00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoQ3Backend1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoQ3Backend1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
